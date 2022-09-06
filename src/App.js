fc488edda1b299b68190399c4ac34de8539b858f
imtiazsamir1 <imtiazsamir416@gmail.com>
2022-09-06 21:21:20 +0600

    bug

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./Components/Pages/Home/Home";
+import Button from "./Components/Pages/Home/Button";
 
 function App() {
   return (
     <div>
       <Home></Home>
+      <Button></Button>
     </div>
   );
 }
