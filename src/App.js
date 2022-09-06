e5d637beda90ffc30b61d6f9717bb161657d92a0
imtiazsamir1 <imtiazsamir416@gmail.com>
2022-09-06 13:15:53 +0600

    daysiUi

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import "./App.css";
 function App() {
   return (
     <div>
-      {" "}
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button class="btn">Button</button>
+      <button class="btn btn-primary">Button</button>
+      <button class="btn btn-secondary">Button</button>
+      <button class="btn btn-accent">Button</button>
+      <button class="btn btn-ghost">Button</button>
+      <button class="btn btn-link">Button</button>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        loginBg: "url('/src/asset/login-bg-1.jpg')",
+      },
+    },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#3b82f6",
+
+          secondary: "#4f46e5",
+
+          neutral: "#242A55",
+
+          "text-main-color": "#333333",
+
+          "text-secondary-color": "#555555",
+
+          "base-100": "#FFFFFF",
+
+          "bottom-footer": "#411361",
+
+          "main-footer": "#4D0C7A",
+
+          contact: "#952DDE",
+
+          border: "#ed3ab26b",
+
+          error: "#fc0303",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 };
